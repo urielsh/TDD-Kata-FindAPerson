7ab76be3dbbfd7dbe9ecf52ba4d40f1ae7decb11
urielsh <uriel.shany@gmail.com>
2015-06-21 20:32:55 +0300

    upup

---
+++ FindAPersonTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Crowdmap():
+	def get_all_posts_for['or'] 
+	pass
+
+class FindPersonTests(unittest.TestCase):
+	def SetUp(self):
+		self.crowdmap = Crowdmap()
+	
+	def test_getAllPostsForName(self):
+		post = crowmap.get_all_posts_for("Or")
+		self.assertIn("Or",posts)
+		
+if __name__=='__main__':
+	unittest.main()
+		
